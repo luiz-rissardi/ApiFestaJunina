ce07fda663b41ffaa7e64531069454d4cc5ce809
luiz-rissardi <rissardi.luiz2006@gmail.com>
2024-03-28 18:57:18 -0300

    CHANGE: return to the basic api

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
-import express from 'express';
+import express, {Router} from 'express';
 import http from "http";
 import { promisify } from 'util';
-import cors from "cors"
+// import cors from "cors"
 import dotenv from "dotenv"
 
 dotenv.config()
 
-import { loggers,bodyParse,RateLimit } from "./server/helpers/helper.js"
+import { loggers, bodyParse, RateLimit } from "./server/helpers/helper.js"
 import { MySqlDatabase } from './server/data/MySqlDataBase.js';
 import { RoutesOfApi } from './server/routes/routes.js';
 import { OrderFactory } from './server/components/order/OrderFactory.js';
@@ -17,34 +17,47 @@ import { UserFactory } from './server/components/users/UserFactory.js';
 import { ClientFactory } from './server/components/clients/clientFactory.js';
 import { CommandFactory } from './server/components/commands/CommandsFactory.js';
 
-export class Server{
-    static createServer(){
+export class Server {
+    static createServer() {
         const app = express();
         const server = http.createServer(app);
-        const routes = Server.#instanceDependeces();
-        const database = MySqlDatabase.build(process.env.CONNECTION_STRING);
+        // const routes = Server.#instanceDependeces();
+        // const database = MySqlDatabase.build(process.env.CONNECTION_STRING);
 
-        app.use(cors({
-            origin: 'http://localhost:4200'
-        }))
-        
-        app.use("/api",RateLimit,bodyParse,routes)
-        server.listen(3000,async ()=>{
-            loggers.info(`Server is running at port 3000`);
-            const events = ["SIGINT","SIGTERM"];
-            events.forEach(event =>{
-                process.on(event,()=>{
-                    Server.#destroyServer(database,server)
-                })
+        // app.use(cors({
+        //     origin: 'http://localhost:4200'
+        // }))
+
+        // app.use("/api",RateLimit,bodyParse,routes)
+        server.listen(3000, () => {
+            console.log(`Server is running at port 3000`);
+            // const events = ["SIGINT","SIGTERM"];
+            // events.forEach(event =>{
+            //     process.on(event,()=>{
+            //         Server.#destroyServer(database,server)
+            //     })
+            // })
+        })
+
+        const routes = Router();
+
+        routes.route("/product").get((req, res) => {
+            res.status(200).json({
+                productName: "pipoca",
+                productId: 3,
+                price: 8.78,
+                quantity: 89
             })
         })
+
+        app.use("/api", routes)
     }
 
-    static async #destroyServer(database,server){
+    static async #destroyServer(database, server) {
         loggers.info("----------BANCO DE DADOS---------")
         loggers.info("fechando banco de dados...");
-        database.end( (err) =>{
-            if(err) 
+        database.end((err) => {
+            if (err)
                 loggers.error("erro ao fechar banco de dados");
             loggers.info("banco de dados fechado!");
         })
@@ -56,14 +69,14 @@ export class Server{
         process.exit(0)
     }
 
-    static #instanceDependeces(){
+    static #instanceDependeces() {
         const orderController = OrderFactory.createInstance();
         const ordersProductController = OrderProdutsFactory.createInstance();
         const productController = ProductFactory.createInstance();
         const userController = UserFactory.createInstance();
         const clientController = ClientFactory.createInstance();
         const commandController = CommandFactory.createInstance();
-        const routes = new RoutesOfApi({ commandController, orderController, ordersProductController, productController, userController,clientController }).getRoutes();
+        const routes = new RoutesOfApi({ commandController, orderController, ordersProductController, productController, userController, clientController }).getRoutes();
         return routes
     }
 }
